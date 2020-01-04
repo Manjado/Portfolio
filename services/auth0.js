9744259d63c1a3a39e37892a8526e7f85643575f
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-01-04 13:00:01 +0100

    JWKS verify token signature

--- services/auth0.js
+++ services/auth0.js
@@ -1,6 +1,7 @@
 import auth0 from 'auth0-js';
 import Cookies from 'js-cookie';
 import jwt from 'jsonwebtoken';
+import axios from 'axios';
 
 class Auth0 {
   constructor() {
@@ -48,6 +49,12 @@ class Auth0 {
     this.auth0.authorize();
   }
 
+  async getJWKS() {
+    const res = await axios.get('https://manjado.auth0.com/.well-known/jwks.json');
+    const jwks = res.data;
+    return jwks;
+  }
+
   logout() {
     Cookies.remove('user');
     Cookies.remove('jwt');
@@ -63,6 +70,24 @@ class Auth0 {
   verifyToken(token) {
     if(token) {
       const decodedToken = jwt.decode(token);
+      const jwks = await this.getJWKS();
+      const jwk = jwks.keys[0];
+      // BUILD CERTIFICATE
+      let cert = jwk.x5c[0];
+      cert = cert.match(/.{1,64}/g).join('\n');
+      cert = `-----BEGIN CERTIFICATE-----\n${cert}-----END CERTIFICATE-----\n`
+
+      if (jwk.kid === decodedToken.header.kid) {
+        try {
+          const verifiedToken = jwt.verify();
+        } catch (err) {
+          return undefined;
+        }
+       
+      }
+
+
+
       const expiresAt = decodedToken.exp * 1000;
 
       return (decodedToken && new Date().getTime() < expiresAt) ? decodedToken : undefined;
