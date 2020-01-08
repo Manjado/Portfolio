8b591e7c851aa293529ed2227073023121960298
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-01-08 21:58:25 +0100

    hoc to check role

--- components/hoc/withAuth.js
+++ components/hoc/withAuth.js
@@ -3,35 +3,36 @@ import BaseLayout from '../layouts/BaseLayout';
 import BasePage from '../BasePage';
 
 export default function(Component) {
-    return class withAuth extends React.Component {
-  
-        static async getInitialProps(args) {
-            const pageProps = await Component.getInitialProps && Component.getInitialProps(args);
+  return class withAuth extends React.Component {
+    static async getInitialProps(args) {
+      const pageProps =
+        (await Component.getInitialProps) && Component.getInitialProps(args);
 
-            return { ...pageProps}
-        }
-
-        renderProtectedPage() {
-            const { isAuthenticated } = this.props.auth;
-      
-            if (isAuthenticated) {
-              return (
-                <Component {...this.props} />
-                )
-            } else {
-              return (
-                  <BaseLayout {...this.props.auth}>
-                    <BasePage>
-                      <h1> You are not authenticated. Please login to acess this page </h1>
-                    </BasePage>
-                  </BaseLayout>
-                )
-            }
-        }
-
-        render() {
-            //return <Component {...this.props}/>
-            return this.renderProtectedPage();
-        }
+      return { ...pageProps };
     }
+
+    renderProtectedPage() {
+      const { isAuthenticated, user } = this.props.auth;
+
+      if (isAuthenticated) {
+        return <Component {...this.props} />;
+      } else {
+        return (
+          <BaseLayout {...this.props.auth}>
+            <BasePage>
+              <h1>
+                {' '}
+                You are not authenticated. Please login to acess this page{' '}
+              </h1>
+            </BasePage>
+          </BaseLayout>
+        );
+      }
+    }
+
+    render() {
+      //return <Component {...this.props}/>
+      return this.renderProtectedPage();
+    }
+  };
 }
