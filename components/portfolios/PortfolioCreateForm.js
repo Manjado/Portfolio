2523f33149d1310c3d72e3ae9c0b84f2b967eb9e
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-01-11 13:29:43 +0100

    more forms

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -3,9 +3,11 @@ import React from 'react';
 export default class PortfolioCreateForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: '' };
+    this.state = { title: '', description: '', language: '' };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this);
+    this.handleChangeLanguage = this.handleChangeLanguage.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -13,7 +15,17 @@ export default class PortfolioCreateForm extends React.Component {
     this.setState({ title: event.target.value });
   }
 
+  handleChangeDescription(event) {
+    this.setState({ description: event.target.value });
+  }
+
+  handleChangeLanguage(event) {
+    console.log(event.target.value, 'select');
+    this.setState({ language: event.target.value });
+  }
+
   handleSubmit(event) {
+    console.log(this.state);
     event.preventDefault();
   }
 
@@ -27,6 +39,22 @@ export default class PortfolioCreateForm extends React.Component {
             value={this.state.value}
             onChange={this.handleChange}
           />
+        </label>
+        <label>
+          Description:
+          <textarea
+            value={this.state.description}
+            onChange={this.handleChangeDescription}
+          />
+        </label>
+        <label>
+          Pick your favorite Programming Language
+          <select value={this.state.value} onChange={this.handleChangeLanguage}>
+            <option value="javascript">JavaScript</option>
+            <option value="php">PHP</option>
+            <option value="java">java</option>
+            <option value="c++">C++</option>
+          </select>
         </label>
         <input type="submit" value="Submit" />
       </form>
