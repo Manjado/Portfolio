d69e721df8c12e87b7d05c1dc66c3921c4153379
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-01-11 13:05:35 +0100

    add portfolio create form

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -2,6 +2,15 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import { Link } from '../routes';
+import {
+  Col,
+  Row,
+  Card,
+  CardHeader,
+  CardBody,
+  CardText,
+  CardTitle
+} from 'reactstrap';
 
 import axios from 'axios';
 
@@ -24,11 +33,32 @@ class Portfolios extends React.Component {
   renderPosts(posts) {
     return posts.map((post, index) => {
       return (
-        <li key={index}>
-          <Link as={`/portfolio/${post.id}`} href="/portfolio/[id]">
-            <a style={{ fontSize: '20px' }}> {post.title} </a>
-          </Link>
-        </li>
+        // <li key={index}>
+        //   <Link as={`/portfolio/${post.id}`} href="/portfolio/[id]">
+        //     <a style={{ fontSize: '20px' }}> {post.title} </a>
+        //   </Link>
+        // </li>
+        <Col md="4">
+          <React.Fragment key={index}>
+            <span>
+              <Card className="portfolio-card">
+                <CardHeader className="portfolio-card-header">
+                  Some Position {index}
+                </CardHeader>
+                <CardBody>
+                  <p className="portfolio-card-city"> Some Location {index} </p>
+                  <CardTitle className="portfolio-card-title">
+                    Some Company {index}
+                  </CardTitle>
+                  <CardText className="portfolio-card-text">
+                    Some Description {index}
+                  </CardText>
+                  <div className="readMore"> </div>
+                </CardBody>
+              </Card>
+            </span>
+          </React.Fragment>
+        </Col>
       );
     });
   }
@@ -38,8 +68,8 @@ class Portfolios extends React.Component {
 
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage title="Portfolios">
-          <ul>{this.renderPosts(posts)}</ul>
+        <BasePage className="portfolio-page" title="Portfolios">
+          <Row>{this.renderPosts(posts)}</Row>
         </BasePage>
       </BaseLayout>
     );
