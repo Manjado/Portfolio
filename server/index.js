dfe6c1186f2a1eb3c119114a0b2cbc0526553b9d
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-01-11 10:57:47 +0100

    ssr role authorization

--- server/index.js
+++ server/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const next = require('next');
 const routes = require('../routes');
 
-// SERVICES 
+// SERVICES
 const authService = require('./services/auth');
-
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -12,30 +11,41 @@ const app = next({ dev });
 const handle = routes.getRequestHandler(app);
 
 const secretData = [
-    {
-        title: 'SecretData 1',
-        description: 'Plan how to build spaceship'
-    },
-    {
-        title: 'SecretData 2',
-        description: 'Plan how to build spaceship'
-    }
-]
+  {
+    title: 'SecretData 1',
+    description: 'Plan how to build spaceship'
+  },
+  {
+    title: 'SecretData 2',
+    description: 'Plan how to build spaceship'
+  }
+];
 
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/api/v1/secret', authService.checkJWT ,(req,res) => {
-      return res.json(secretData);
+  server.get('/api/v1/secret', authService.checkJWT, (req, res) => {
+    return res.json(secretData);
   });
+
+  server.get(
+    '/api/v1/onlysiteowner',
+    authService.checkJWT,
+    authService.checkRole('siteOwner'),
+    (req, res) => {
+      return res.json(secretData);
+    }
+  );
 
   server.get('*', (req, res) => {
     return handle(req, res);
   });
 
-  server.use(function (err, req, res, next) {
+  server.use(function(err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
-      res.status(401).send({title: 'Unauthorized', detail: 'Unauthorized Access!'});
+      res
+        .status(401)
+        .send({ title: 'Unauthorized', detail: 'Unauthorized Access!' });
     }
   });
 
--- server/services/auth.js
+++ server/services/auth.js
@@ -1,14 +1,28 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
-
+const namespace = 'http://localhost:3000/';
 // MIDDLEWARE
-exports.checkJWT = jwt({ secret: jwksRsa.expressJwtSecret({
+exports.checkJWT = jwt({
+  secret: jwksRsa.expressJwtSecret({
     cache: true,
     jwksUri: 'https://manjado.auth0.com/.well-known/jwks.json',
     rateLimit: true,
     jwksRequestsPerMinute: 15
   }),
-audience: 'VSrdZd7W3m1hn5Bp2JABIU6bn7Bul0IW',
-issuer: 'https://manjado.auth0.com/',
-algorithms: ['RS256']
-})
+  audience: 'VSrdZd7W3m1hn5Bp2JABIU6bn7Bul0IW',
+  issuer: 'https://manjado.auth0.com/',
+  algorithms: ['RS256']
+});
+
+exports.checkRole = role => (req, res, next) => {
+  const user = req.user;
+
+  if (user && user[namespace + 'role'] === role) {
+    next();
+  } else {
+    return res.status(401).send({
+      title: 'Not Autorized',
+      detail: 'You are not authorized to acces this data'
+    });
+  }
+};
