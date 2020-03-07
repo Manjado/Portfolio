88851d07272e9e08c0ba98dda306db613bb739f6
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-03-07 11:36:13 +0100

    fix error with nextjs destroy and net

--- actions/index.js
+++ actions/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 import { getCookieFromReq } from "../helpers/utils";
-import { response } from "express";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000/api/v1",
